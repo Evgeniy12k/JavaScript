61f0b16b4b8ca576a5f183b84c622d8bd3ab57d6
Evgeniy Kulikov <evgenii49-9@yndex.ru>
2020-10-11 00:30:51 +0300

    доп.задание

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var num = 266219;
+
+ otvet=num.toString().split('').reduce((res, item) => item * res, 1);
+ console.log(otvet);
+ console.log(otvet**=3);
+
+ console.log( String(otvet).slice(0,2));
